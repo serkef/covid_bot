24f5bceb738ee5bfb99b9bba532975b912d15e19
Sergio Kef <sergiokef@gmail.com>
2020-03-14 23:00:25 +0100

    Fix hashtag

--- gsheet_bot/config.py
+++ gsheet_bot/config.py
@@ -45,7 +45,7 @@ DB_INSERT_RAW_DAILY_DATA = Path(__file__).parent / "sql" / "insert_raw_daily_dat
 DB_INSERT_RAW_HOME_DATA = Path(__file__).parent / "sql" / "insert_raw_home_data.sql"
 
 
-STATUS_HEADER = "#BREAKING latest #COVID-19 #CoronavirusPandemic update"
+STATUS_HEADER = "#BREAKING latest #COVIDー19 #CoronavirusPandemic update"
 STATUS_FOOTER = "Visit 📊covid2019.app for the latest updates"
 
 POST_TWITTER = os.getenv("POST_TWITTER", "false") == "true"
